134d72583d30c5383ef298f2b87774538e41f1f8
Treevyy <aarontrevormoore@gmail.com>
2025-08-03 17:19:12 -0500

    created a while loop for user to choose to keep playing, ask for user choice for R, P, S, user has option to cancel and end the game aka break the while loop, added toUpperCase so user is not forced to input in uppercase, and added a stats counter for overall options

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ const playGame = function() {
 
     const options = ['R', 'P', 'S'];
 
+    let keepPlaying = true;
+
+    while(keepPlaying) {
+        let userChoice = window.prompt('Enter R, P, or S:')
+
+        if (!userChoice) {
+            return;
+        }
+
+        userChoice = userChoice.toUpperCase();
+        if(!options.includes(userChoice))  {
+            window.alert('Please enter a valid choice.')
+        } else {
+            switch(userChoice){
+                case 'R':
+                    stats.count.rock++;
+                    break;
+                case 'P':
+                    stats.count.paper++;
+                    break;
+                case 'S':
+                    stats.count.scissors++;
+                    break;
+            }
+        }
 }
 
 playGame();
