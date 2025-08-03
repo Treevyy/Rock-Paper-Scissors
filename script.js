184f9873a9e020cb189c6225e5bd9c6d737b0dfb
Treevyy <aarontrevormoore@gmail.com>
2025-08-03 17:07:46 -0500

    created an object to track player stats

---
+++ script.js
@@ -0,0 +1,13 @@
+const playGame = function() {
+    const stats = {
+        wins: 0,
+        ties: 0,
+        losses: 0,
+        count: {
+            rock: 0,
+            paper: 0,
+            scissors: 0,
+        }
+    }
+}
+playGame();
